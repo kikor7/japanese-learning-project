c43a0d6d2fe350888665e0e73f73ac7b9e103a68
kikor7 <kiko2002@hotmail.es>
2026-02-05 13:23:51 +0100

    prueba

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,27 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+mounted() {
+  
+  // Traemos la api key
+  const mi = import.meta.env.VITE_MI_API_KEY; 
+  
+// La URL de la API (el sitio que da la info)
+  const urlApi = 'https://newsapi.org/v2/everything?q=tesla&from=2026-01-05&sortBy=publishedAt&apiKey=VITE_MI_API_KEY'; 
+
+  axios.get(urlApi, {
+    headers: {
+      'Authorization': `Bearer ${miLlave}`
+    }
+  })
+  .then(response => {
+    this.info = response.data;
+  })
+  .catch(error => console.error(error));
+}
+</script>
 
 <template>
   <main>
-    <header class="bg-gradient-to-r from-[#b3daff] to-[#edf3ff] w-full h-20 items-center flex justify-start px-4">
+    <header class="bg-linear-to-r from-[#b3daff] to-[#edf3ff] w-full h-20 items-center flex justify-start px-4">
       <img class="w-20 h-20" alt="Japanese logo" src="./assets/img/Japanese_icon_for_user_box.png" />
       <p class="text-3xl font-bold text-white ml-4">Aprende Japonés</p>
       <div class="flex justify-end w-full ">
@@ -20,7 +39,7 @@
   
     </div>
 
-  <div class="bg-gradient-to-r from-[#b3daff] to-[#edf3ff] w-full h-10 px-4 fixed bottom-0 left-50 max-w-6xl mx-auto">
+  <div class="bg-linear-to-r from-[#b3daff] to-[#edf3ff] w-full h-10 px-4 fixed bottom-0  max-w-6xl mx-auto">
     <footer>
       <div class="flex items-end justify-end">
       <p class="text-white font-semibold">© 2026 Aprende Japonés. Todos los derechos reservados.</p>
