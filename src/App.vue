8afc3d97c5041d09912490a6b4d908d89724e103
kikor7 <kiko2002@hotmail.es>
2026-02-10 16:03:12 +0100

    petalos animados background funcionando

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,23 @@
 <script setup lang="ts">
-import type router from './router';
+import { computed } from 'vue'
+import { useRoute } from 'vue-router'
+import petalosBackground from '@/components/petalosBackground.vue';
+import headerPagina from '@/components/headerPagina.vue';
+import footerPagina from '@/components/footerPagina.vue';
+
+const route = useRoute()
+
+const tipoHeader = computed(() =>
+  route.name === 'login' ? 'login' : 'principal'
+)
+
 
 </script>
 
 
 
 <template>
+  <petalosBackground />
  <headerPagina tipo="principal" />
 
   <router-view />
---
+++ src/components/petalosBackground.vue
@@ -0,0 +1,47 @@
+<template>
+  <vue-particles
+    id="tsparticles"
+    :options="particlesOptions"
+  />
+</template>
+
+<script setup>
+const particlesOptions = {
+  particles: {
+    number: { value: 30 }, // No satures la pantalla
+    color: { value: "#ffb7c5" }, // Rosa sakura
+    shape: { type: "circle" }, // Puedes usar una imagen de pétalo luego
+    opacity: { value: 0.7 },
+    size: { value: { min: 2, max: 5 } },
+    move: {
+      enable: true,
+      speed: 1.5,
+      direction: "bottom", // Caen hacia abajo
+      random: true,
+      straight: false, // ¡Importante! Esto activa el movimiento fluido
+      outModes: { default: "out" },
+    },
+    wobble: { // Esto imita el balanceo del pétalo al caer
+      enable: true,
+      distance: 10,
+      speed: 10
+    }
+  },
+  interactivity: {
+    events: {
+      onHover: { enable: true, mode: "bubble" } // Reaccionan un poco al ratón
+    }
+  }
+};
+</script>
+
+<style scoped>
+#tsparticles {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  z-index: -1; /* Para que esté detrás de tu contenido */
+}
+</style>
